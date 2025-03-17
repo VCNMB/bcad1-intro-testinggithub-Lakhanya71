9de5e3a47a57f22106f80cc6cc3e80a08c1152b1
Lakhanya71 <lakhanyasmith@gmail.com>
2025-03-17 10:35:37 +0200

    Add files via upload

---
+++ PrjClasses1.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.prjclasses1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Lakhanya Smith
+ */
+public class PrjClasses1 {
+
+    private static void main(String[] args) {
+            
+            String strName = JOptionPane.showInputDialog(null, "Please enter student name");
+            String strCourse = JOptionPane.showInputDialog(null, "Please enter student course");
+            double dblAvg = Double.parseDouble(JOptionPane.showInputDialog(null, "Please enter student average result"));
+            
+            s.setName(strName);
+            s.setCourse (strCourse);
+            s.setAverage (dblAvg);
+            print();
+     
+        }
+        public static void print()
+    }
+        String strDisplay = "The updated student average results are: \n"" +"
++ "
+    }
+
